8b302948cd38554b30ac11f6b40f12997802507e
Akash1-OP <akashsahana62@gmail.com>
2026-04-29 02:41:29 +0530

    removed .env from tracking

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AppProvider } from "./context/AppContext.tsx";
+import "leaflet/dist/leaflet.css";
+import { SocketProvider } from "./context/SocketContext.tsx";
+
+export const authService = "https://aomato-auth.onrender.com";
+export const restaurantService = "https://aomato-restaurant.onrender.com";
+export const utilsService = "https://aomato-utils.onrender.com";
+export const realtimeService = "https://aomato-realtime.onrender.com";
+export const riderService = "https://aomato-rider.onrender.com";
+export const adminService = "https://aomato-admin.onrender.com";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <GoogleOAuthProvider clientId="962632561697-fsfne5anseqmgtdiqjs5tsghnj4obiiv.apps.googleusercontent.com">
+      <AppProvider>
+        <SocketProvider>
+          <App />
+        </SocketProvider>
+      </AppProvider>
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
